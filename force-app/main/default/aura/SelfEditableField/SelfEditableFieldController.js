d48c4ed4b56371a76b72f946c735ca2b04e49e72
Rodrigo Juliani <rjuliani@altimetrik.com>
2019-01-25 15:33:35 -0300

    Introduced helper concepts passed saveValue and toggleEdit to helper. Called helper from action handlers in controller

--- force-app/main/default/aura/SelfEditableField/SelfEditableFieldController.js
+++ force-app/main/default/aura/SelfEditableField/SelfEditableFieldController.js
@@ -9,18 +9,9 @@
         component.set('v.tempValue', component.get('v.value'));
     },
     toggleEdit : function(component, event, helper) {
-        // get the current value of the "editMode" attribute
-        var editMode = component.get('v.editMode');
-        // toggle the value
-        editMode = !editMode;
-        // set the attribute so the view gets refreshed. 
-        component.set('v.editMode', editMode);
+        helper.toggleEdit(component);
     },
     saveValue : function(component, event, helper) {
-        console.log('saving');
-        component.set('v.value', component.get('v.tempValue'));
-        // need to call toggleEdit... 
-        // but can't call a controller method from the controller!!
-        
+        helper.saveValue(component);
     }
 })
--- force-app/main/default/aura/SelfEditableField/SelfEditableFieldHelper.js
+++ force-app/main/default/aura/SelfEditableField/SelfEditableFieldHelper.js
@@ -1,5 +1,21 @@
+/**
+ * Helper is like a service: 
+ *  - Static
+ *  - Shared between all instances of the same component.
+ *  - Does heavylifting
+ *  - Can call its own methods! (AKA "this" is a thing!)
+ */
 ({
-    helperMethod : function() {
-
+    toggleEdit : function(component) {
+        // get the current value of the "editMode" attribute
+        var editMode = component.get('v.editMode');
+        // toggle the value
+        editMode = !editMode;
+        // set the attribute so the view gets refreshed. 
+        component.set('v.editMode', editMode);
+    },
+    saveValue : function(component) {
+        component.set('v.value', component.get('v.tempValue'));
+        this.toggleEdit(component);
     }
 })
